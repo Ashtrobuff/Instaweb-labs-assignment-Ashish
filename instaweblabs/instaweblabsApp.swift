bd6b634e934b22044631e91ae25d8f6984690ec3
ashish <ashishp1729@gmail.com>
2025-06-09 16:08:52 +0530

    Initial Commit

---
+++ instaweblabs/instaweblabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaweblabsApp.swift
+//  instaweblabs
+//
+//  Created by Ashish on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct instaweblabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
